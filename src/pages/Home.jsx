03ae3a3ecba9f3cdea6459024fb7941ce5501c2f
aldisypu <aldisypu@gmail.com>
2024-01-08 22:47:23 +0700

    add card home pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,45 @@ export default function Home() {
     return (
     <>
         <Header />
-        <h1>Home Page</h1>
+        <div className="container mx-auto px-6 sm:flex sm:flex-wrap sm:gap-6 sm:justify-evenly">
+            <a href="/overlays" className="mb-6 cursor-pointer transition-all duration-500 hover:translate-y-2 w-full h-40 sm:w-2/5 bg-neutral-50 rounded-lg ring-2 ring-purple-300 shadow-xl shadow-purple-300 flex flex-row items-center justify-evenly gap-4 px-4">
+                <div>
+                    <span className="font-bold text-purple-300">Overlay</span>
+                    <p className="line-clamp-3">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                </div>
+            </a>
+            <a href="/overlays" className="mb-6 cursor-pointer transition-all duration-500 hover:translate-y-2 w-full h-40 sm:w-2/5 bg-neutral-50 rounded-lg ring-2 ring-purple-300 shadow-xl shadow-purple-300 flex flex-row items-center justify-evenly gap-4 px-4">
+                <div>
+                    <span className="font-bold text-purple-300">Overlay</span>
+                    <p className="line-clamp-3">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                </div>
+            </a>
+            <a href="/overlays" className="mb-6 cursor-pointer transition-all duration-500 hover:translate-y-2 w-full h-40 sm:w-2/5 bg-neutral-50 rounded-lg ring-2 ring-purple-300 shadow-xl shadow-purple-300 flex flex-row items-center justify-evenly gap-4 px-4">
+                <div>
+                    <span className="font-bold text-purple-300">Overlay</span>
+                    <p className="line-clamp-3">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                </div>
+            </a>
+            <a href="/overlays" className="mb-6 cursor-pointer transition-all duration-500 hover:translate-y-2 w-full h-40 sm:w-2/5 bg-neutral-50 rounded-lg ring-2 ring-purple-300 shadow-xl shadow-purple-300 flex flex-row items-center justify-evenly gap-4 px-4">
+                <div>
+                    <span className="font-bold text-purple-300">Overlay</span>
+                    <p className="line-clamp-3">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua.
+                    </p>
+                </div>
+            </a>
+        </div>
+
         <Footer />
     </>
     );
